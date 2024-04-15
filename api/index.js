cf1cbf52983a0111f807fb0f92b8d1351343ab90
SudhanshuVerma <2003sudhanshucool@gmail.com>
2024-04-15 16:13:00 +0530

    testing the server and make errorhandler functoin

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from "dotenv"
-
+import userRouter from './routes/user.route.js'
 
 dotenv.config()
 mongoose.connect(process.env.MONGO).then(()=>
@@ -11,7 +11,7 @@ mongoose.connect(process.env.MONGO).then(()=>
     console.log(error)
 })
  const app= express()
-
+  app.use(express.json())
 
  
 
@@ -19,4 +19,20 @@ mongoose.connect(process.env.MONGO).then(()=>
     console.log("server is running on port 3000")
  })
 
- 
+
+app.use('/api/user',userRouter)
+
+
+
+
+
+app.use((err,req,res,next)=>{
+    const statusCode= err.statusCode || 500;
+    const message= err.message || "Internal server error"
+    res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message
+    })
+}
+    )
